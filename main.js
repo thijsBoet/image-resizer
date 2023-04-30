1e9296a0835b3c3ccbd26f7dbda0d85a790709e8
thijsBoet <m.boet@chello.nl>
2023-05-01 00:24:23 +0200

    finished basic functionality, starting packaging app for multiple platforms

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
+const os = require('os');
+const fs = require('fs');
+const resizeImg = require('resize-img');
 const path = require('path');
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
+
+process.env.NODE_ENV = 'production';
 
 const isDev = process.env.NODE_ENV !== 'production';
 const isMac = process.platform === 'darwin';
 // const isWin = process.platform === 'win32';
 // const isLinux = process.platform === 'linux';
 
+let mainWindow;
+
 function createMainWindow() {
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         title: 'Image Resizer',
         width: isDev ? 1147 : 500,
         height: isDev ? 1440 : 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: true,
+        }
     });
 
     // open dev tools when in dev environment
@@ -38,6 +50,8 @@ app.whenReady().then(() => {
 
     const mainMenu = Menu.buildFromTemplate(menu);
     Menu.setApplicationMenu(mainMenu);
+
+    mainWindow.on('closed', () => mainWindow = null);
 
 	app.on('activate', () => {
 		if (BrowserWindow.getAllWindows().length === 0) {
@@ -81,6 +95,35 @@ const menu = [
     //     ]
     // }
 ]
+
+// Respond to ipcRenderer
+ipcMain.on('image:resize', (e, options) => {
+    options.dest = path.join(os.homedir(), 'imageresizer');
+    resizeImage(options);
+});
+
+async function resizeImage({ imgPath, width, height, dest }) {
+	try {
+		const newPath = await resizeImg(fs.readFileSync(imgPath), {
+			width: +width,
+			heigth: +height,
+		});
+
+        const filename = path.basename(imgPath);
+
+        if (!fs.existsSync(dest)) {
+            fs.mkdirSync(dest);
+        }
+
+        fs.writeFileSync(path.join(dest, filename), newPath);
+
+        mainWindow.webContents.send('image:done');
+
+        shell.openPath(dest);
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 app.on('window-all-closed', () => {
     if (!isMac) {
--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -6,29 +6,86 @@ const heightInput = document.querySelector('#height');
 const widthInput = document.querySelector('#width');
 
 function loadImage(e) {
-    const file = e.target.files[0];
+	const file = e.target.files[0];
 
-    if (!isFileImage(file)) {
-        alert('Image is not of type gif, png or jpeg, please select another image type.');
-        return
-    }
+	if (!isFileImage(file)) {
+		alertError(
+			'Image is not of type gif, png or jpeg, please select another image.',
+		);
+		return;
+	}
 
-    const image = new Image();
-    image.src = URL.createObjectURL(file);
-    image.onload = function () {
-        heightInput.value = this.height;
-        widthInput.value = this.width;
-    }
+	const image = new Image();
+	image.src = URL.createObjectURL(file);
+	image.onload = function () {
+		heightInput.value = this.height;
+		widthInput.value = this.width;
+	};
 
-    form.style.display = 'block';
-    filename.innerText = file.name;
-
+	form.style.display = 'block';
+	filename.innerText = file.name;
+	outputPath.innerText = path.join(os.homedir(), 'imageresizer');
 }
 
-function isFileImage(file) {
-    const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+function sendImage(e) {
+    e.preventDefault();
 
-    return file && acceptedImageTypes.includes(file['type']);
+    const height = heightInput.value;
+    const width = widthInput.value;
+    const imgPath = img.files[0].path;
+
+    if (!img.files[0]) {
+        alertError('Please upload an image.');
+        return;
+    }
+
+    if (height === '' || width === '') {
+        alertError('Please enter a height and width.');
+        return;
+    }
+
+    ipcRenderer.send('image:resize', {
+        imgPath,
+        width,
+        height,
+    });
+}
+
+ipcRenderer.on('image:done', (e, message) => {
+    alertSuccess(`Image resized successfully to ${widthInput.value} x ${heightInput.value}.`);
+});
+
+function isFileImage(file) {
+	const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+
+	return file && acceptedImageTypes.includes(file['type']);
+}
+
+function alertError(message) {
+	Toastify.toast({
+		text: message,
+		duration: 5000,
+		close: false,
+		style: {
+			background: 'red',
+			color: 'white',
+			textAlign: 'center',
+		},
+	});
+}
+
+function alertSuccess(message) {
+	Toastify.toast({
+		text: message,
+		duration: 5000,
+		close: false,
+		style: {
+			background: 'blue',
+			color: 'white',
+			textAlign: 'center',
+		},
+	});
 }
 
 img.addEventListener('change', loadImage);
+form.addEventListener('submit', e => sendImage(e));
